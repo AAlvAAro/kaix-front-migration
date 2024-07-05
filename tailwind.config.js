710b83cd5ef1e0c93864ba8376e868a3d62386b7
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2024-07-05 01:26:53 +0000

    [ADD] Deposito Module Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors:{
+                'primary': '#ffffff',
+                'green': '#93F775',
+                'darkGreen': '#164e05',
+                'purple':'#9E70D8',
+                'background': '#000000',
+                'card':'#222222',
+                'cardHover':'#4D4D4D',
+                'lightTeal':'#94DBDA',
+                'orange':'#AE563A',
+                'inputBackground':'#f2f2f2',
+                'inputText':'#8a8a8a',
+                'chartLine':'#E4E4E433'
+              },
+        },
     },
     plugins: [],
 }
